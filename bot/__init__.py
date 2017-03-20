6c197eb63475a06b5ce31e291be9f57676b69dd0
Gates_ice <gates@gatesice.com>
2017-03-20 17:45:59 -0500

    Auto save photo functionality.

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,6 +4,7 @@ from telegram.ext import Updater, MessageHandler, Filters
 
 from .config import config
 from .models import save_photo_record, save_text_message
+from .handlers import save_photo
 
 logger = logging.getLogger(__name__)
 
@@ -15,11 +16,6 @@ def echo(bot, update):
         update.message.text)
     )
     save_text_message(update.message)
-    
-
-def echo_photo(bot, update):
-    print("{} sent a photo".format(update.message.from_user.name))
-    save_photo_record(update.message, update.message.photo[0], "123", "234", "134")
 
 
 def start():
@@ -29,7 +25,7 @@ def start():
     echo_handler = MessageHandler(Filters.text, echo)
     updater.dispatcher.add_handler(echo_handler)
 
-    photo_handler = MessageHandler(Filters.photo, echo_photo)
+    photo_handler = MessageHandler(Filters.photo, save_photo.handler)
     updater.dispatcher.add_handler(photo_handler)
 
     updater.start_polling()
--- bot/handlers/save_photo.py
+++ bot/handlers/save_photo.py
@@ -1,5 +1,46 @@
+import os
+from sys import exc_info
+# from io import BytesIO
+from uuid import uuid4
+from telegram import File
+import logging
 
+from ..models import save_photo_record
+from ..cloud.cloud_storage import upload_to_cloud_storage
+from ..config import config
+
+logger = logging.getLogger(__name__)
+base_path = os.path.expanduser(config.get("SavePhotoHandler", "BasePath"))
+if not os.path.exists(base_path):
+    os.makedirs(base_path, exist_ok=True)
+
+
+def file_path(filename):
+    return os.path.join(base_path, filename)
+
+def cloud_file_path(filename):
+    return "photos/{}".format(filename)
 
 def handler(bot, update):
     print("{} sent a photo".format(update.message.from_user.name))
+
+    if update.message.photo:
+        for ps in update.message.photo:
+            try:
+                f = bot.getFile(ps.file_id)
+                print(f)
+                print(dir(f))
+                file_uuid = str(uuid4()).upper()
+                file_ext = f.file_path.split('/')[-1].split('.')[-1]
+                file_name = "{}.{}".format(file_uuid, file_ext)
+                file_full_path = file_path(file_name)
+                f.download(custom_path=file_full_path)
+                fp = open(file_full_path, 'rb')
+                fc = fp.read()
+                fp.close()
+                url = upload_to_cloud_storage(fc, cloud_file_path(file_name))
+                save_photo_record(update.message, ps, file_name, file_uuid, url)
+            except:
+                logger.exception(exc_info()[0])
+
     
